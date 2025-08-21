a338f9013675dd7bab46a31fdbf1d6f9db28af7e
Gustavoff08 <gffreiremkt@gmail.com>
2025-08-21 10:45:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 // server.js
-import express from "express";
-import bodyParser from "body-parser";
-import fetch from "node-fetch";
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
 
 const app = express();
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
-const VERIFY_TOKEN     = "copydigital123";
-const WHATSAPP_TOKEN   = "EAAKf0evy6agBPAjsA8n9j5LmOivtlfEJwQ5yOCLzv2TveOnSSGvOjXvjv046qoS7eXWmhWYy64YMzqE6Fd82mr1O5npbIRVxocNHqnR2ZBN8RyZBZAxAvJLNZCXKYMHhiXjnS90akVzgZAGVoTZAZAKkrEe7ldc3GXYnmg4hmY0oZCpktXt1RqYcHyVoWZC23K2Sc9vIydBEAAWzZCcwpcRi9fj402jMsZBUiMxpuyPnBHjFE30WTRJE0IUhNKfnNtaZAQZDZD
-";
-const PHONE_NUMBER_ID  = "175456228055578";
-const LEAD_WEBHOOK_URL = ""; // se for usar Zapier/Make, coloque a URL aqui
+
+const VERIFY_TOKEN = "copydigital123"; 
+const WHATSAPP_TOKEN = "EAAKf0evy6agBPAjsA8n9j5LmOivtlfEJwQ5yOCLzv2TveOnSSGvOjXvjv046qoS7eXWmhWYy64YMzqE6Fd82mr1O5npbIRVxocNHqnR2ZBN8RyZBZAxAvJLNZCXKYMHhiXjnS90akVzgZAGVoTZAZAKkrEe7ldc3GXYnmg4hmY0oZCpktXt1RqYcHyVoWZC23K2Sc9vIydBEAAWzZCcwpcRi9fj402jMsZBUiMxpuyPnBHjFE30WTRJE0IUhNKfnNtaZAQZDZD
+"; 
+const PHONE_NUMBER_ID = "175456228055578"; 
+const LEAD_WEBHOOK_URL = ""; // opcional
 
 
 // ============ Sessões em memória (para testes) ============
