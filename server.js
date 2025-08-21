d0a678ca023cbce2339c028af98379771f8fe96e
Gustavoff08 <gffreiremkt@gmail.com>
2025-08-21 10:16:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
-const VERIFY_TOKEN = process.env.VERIFY_TOKEN;           // ex: copysuporte
-const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN;       // token do meta
-const PHONE_NUMBER_ID = process.env.PHONE_NUMBER_ID;     // id do número
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN copydigital123;           // ex: copysuporte
+const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN EAAKf0evy6agBPAjsA8n9j5LmOivtlfEJwQ5yOCLzv2TveOnSSGvOjXvjv046qoS7eXWmhWYy64YMzqE6Fd82mr1O5npbIRVxocNHqnR2ZBN8RyZBZAxAvJLNZCXKYMHhiXjnS90akVzgZAGVoTZAZAKkrEe7ldc3GXYnmg4hmY0oZCpktXt1RqYcHyVoWZC23K2Sc9vIydBEAAWzZCcwpcRi9fj402jMsZBUiMxpuyPnBHjFE30WTRJE0IUhNKfnNtaZAQZDZD;       // token do meta
+const PHONE_NUMBER_ID = process.env.PHONE_NUMBER_ID 725458220655578;     // id do número
 const LEAD_WEBHOOK_URL = process.env.LEAD_WEBHOOK_URL || ""; // opcional (Zapier/Make/CRM)
 
 // ============ Sessões em memória (para testes) ============
